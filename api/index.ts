f356e6b735d316509924d0cd34258ab14de4088e
DemaPy <benderdema@gmail.com>
2024-10-21 21:33:28 +0200

    setup vercel

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,45 @@
-const _app = require("./index.ts")
+const { config } = require("dotenv");
+config();
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const express = require("express");
+const cors = require("cors");
+const {
+  templateRouter,
+  sectionRouter,
+  componentRouter,
+  authRouter,
+  campaignRouter,
+  layoutRouter,
+  userRouter,
+} = require("./routes");
+const { handlePrismaError, logErrors } = require("./middlewares/errorHandling");
+const app = express();
 
-module.exports = _app
+var corsOptions = {
+  origin: process.env.FRONTEND_DEV,
+  credentials: true,
+  methods: "GET,PUT,POST,OPTIONS,DELETE,PATCH",
+  allowedHeaders: "Content-Type,Authorization",
+};
+app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use(express.json({ limit: "10mb" }));
+
+app.use("/templates", templateRouter);
+app.use("/sections", sectionRouter);
+app.use("/components", componentRouter);
+app.use("/campaigns", campaignRouter);
+app.use("/layouts", layoutRouter);
+app.use("/auth", authRouter);
+app.use("/user", userRouter);
+app.get("/", (req, res) => res.send('Welcome to my app.<br/> <a href="https://github.com/DemaPy">Author</a>'));
+
+app.use(logErrors);
+app.use(handlePrismaError);
+
+app.listen(3000, () => {
+  console.log("Server started at port:", 3000);
+});
+
+module.exports = app;
